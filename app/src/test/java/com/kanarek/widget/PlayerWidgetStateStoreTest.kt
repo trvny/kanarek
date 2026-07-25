eac7236916e2ca54842645011decc614080a23eb
trvny <120686325+trvny@users.noreply.github.com>
2026-07-25 22:59:53 +0200

    kanarek: run widget state tests on supported SDK

--- app/src/test/java/com/kanarek/widget/PlayerWidgetStateStoreTest.kt
+++ app/src/test/java/com/kanarek/widget/PlayerWidgetStateStoreTest.kt
@@ -8,8 +8,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34])
 class PlayerWidgetStateStoreTest {
     private val store: PlayerWidgetStateStore
         get() = PlayerWidgetStateStore(RuntimeEnvironment.getApplication())
